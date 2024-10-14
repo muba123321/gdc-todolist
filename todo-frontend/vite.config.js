796f573db08040eeab0f05832f8104a9e5c2410c
muba123321/ <laarychambas@gmail.com>
2024-10-14 04:16:09 -0400

    setting up frontend for deployment

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -3,15 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3500",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  server: !isProduction
+    ? {
+        proxy: {
+          "/api": {
+            target: "http://localhost:3500",
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      }
+    : {},
 
   plugins: [react()],
 });
