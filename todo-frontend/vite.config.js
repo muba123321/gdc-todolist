6957ca83dc25cf9f901eb9124935bea98260679b
muba123321/ <laarychambas@gmail.com>
2024-10-14 04:24:01 -0400

    modfied viteconfig

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: !isProduction
