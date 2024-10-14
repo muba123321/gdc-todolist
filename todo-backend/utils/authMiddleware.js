8f2cd687d7d8ded9f8fcabbde2123e8c1b38469b
muba123321/ <laarychambas@gmail.com>
2024-10-14 16:13:45 -0400

    added some logs

--- todo-backend/utils/authMiddleware.js
+++ todo-backend/utils/authMiddleware.js
@@ -2,14 +2,18 @@ import jwt from "jsonwebtoken";
 
 // function to authenticate User bofore accessing any api
 const authenticate = (req, res, next) => {
+  console.log(req.cookies);
   console.log(req.cookies.access_token);
   const token = req.cookies.access_token;
+  console.log(token);
 
   if (!token) return res.status(401).send({ message: "Access denied" });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    console.log(decoded);
     req.user = decoded;
+    console.log(req.user);
 
     next();
   } catch (err) {
